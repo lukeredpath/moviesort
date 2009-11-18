294fb10c1b02fa31250fbf67e823a31a6214aec0
Luke Redpath <contact@lukeredpath.co.uk>
2009-11-18 17:48:02 +0000

    Support for multiple movie types

--- lib/moviesort/command_line.rb
+++ lib/moviesort/command_line.rb
@@ -7,6 +7,8 @@ module Moviesort
       new(options.split)
     end
     
+    MOVIE_EXTENSIONS = %w{avi mkv mov mp4}
+    
     def initialize(argv = ARGV)
       @options = Trollop.options(argv) do
         banner <<-EOS.gsub(/^ {8}/, '')
@@ -22,7 +24,10 @@ module Moviesort
     
     def run
       sorter = Sorter::TV.new(@options[:target])
-      Dir[File.join(@options[:source], '*.avi')].each do |file|
+      movie_files = Dir[File.join(@options[:source], '*')].select do |file|
+        MOVIE_EXTENSIONS.include?(File.extname(file)[1..-1])
+      end
+      movie_files.each do |file|
         sorter.sort_file(file)
       end
     end
