2f7ec4e394105f8716bdd228f2791f61543c5b0f
Luke Redpath <contact@lukeredpath.co.uk>
2009-11-18 22:22:15 +0000

    Move folder traversal into the sorter

--- lib/moviesort/command_line.rb
+++ lib/moviesort/command_line.rb
@@ -7,8 +7,6 @@ module Moviesort
       new(options.split)
     end
     
-    MOVIE_EXTENSIONS = %w{avi mkv mov mp4}
-    
     def initialize(argv = ARGV)
       @options = Trollop.options(argv) do
         banner <<-EOS.gsub(/^ {8}/, '')
@@ -24,12 +22,7 @@ module Moviesort
     
     def run
       sorter = Sorter::TV.new(@options[:target])
-      movie_files = Dir[File.join(@options[:source], '*')].select do |file|
-        MOVIE_EXTENSIONS.include?(File.extname(file)[1..-1])
-      end
-      movie_files.each do |file|
-        sorter.sort_file(file)
-      end
+      sorter.sort_directory(@options[:source])
     end
   end
 end
--- lib/moviesort/sorter.rb
+++ lib/moviesort/sorter.rb
@@ -3,8 +3,16 @@ require 'moviesort/tv_show'
 module Moviesort
   module Sorter
     class TV
+      VALID_FILE_TYPES = %w{avi mkv mov mp4}
+      
       def initialize(output_dir)
         @output_dir = output_dir
+      end
+      
+      def sort_directory(path)
+        movie_files_in_directory(path).each do |file|
+          sort_file(file)
+        end
       end
       
       def sort_file(path)
@@ -12,6 +20,14 @@ module Moviesort
         target_path = File.join(@output_dir, tv_show.target_path)
         FileUtils.mkdir_p(target_path)
         FileUtils.mv(path, File.join(target_path, tv_show.target_filename))
+      end
+      
+      private
+      
+      def movie_files_in_directory(path)
+        Dir[File.join(path, '*')].select do |file|
+          VALID_FILE_TYPES.include?(File.extname(file)[1..-1])
+        end
       end
     end
   end
