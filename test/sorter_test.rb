c1aeb6b81eccf5ad5eca2284da94c73c480321aa
Luke Redpath <contact@lukeredpath.co.uk>
2009-11-18 22:31:53 +0000

    Add some tests for the file extensions

--- test/sorter_test.rb
+++ test/sorter_test.rb
@@ -4,10 +4,16 @@ require 'moviesort/sorter'
 class SorterTest < Test::Unit::TestCase
   
   def setup
+    FakeFS.activate!
     @output_path   = "/output"
     @download_path = "/downloads"
     FileUtils.mkpath(@output_path)
     FileUtils.mkpath(@download_path)
+  end
+  
+  def teardown
+    FakeFS::FileSystem.clear
+    FakeFS.deactivate!
   end
 
   context "TV show sorter" do
@@ -19,6 +25,21 @@ class SorterTest < Test::Unit::TestCase
       FileUtils.touch("/downloads/The Sopranos - S02E04.avi")
       @sorter.sort_file("/downloads/The Sopranos - S02E04.avi")
       assert File.exist?("/output/The Sopranos/Season 2/The.Sopranos.S02E04.avi")
+    end
+    
+    should "sort all valid movie files in a given directory" do
+      %w{avi mp4 mov mkv}.each { |ext| FileUtils.touch("/downloads/movie.#{ext}") }
+      @sorter.expects(:sort_file).with("/downloads/movie.avi")
+      @sorter.expects(:sort_file).with("/downloads/movie.mkv")
+      @sorter.expects(:sort_file).with("/downloads/movie.mp4")
+      @sorter.expects(:sort_file).with("/downloads/movie.mov")
+      @sorter.sort_directory(@download_path)
+    end
+    
+    should "ignore non-valid movie files in a given directory" do
+      FileUtils.touch("/downloads/movie.mp3")
+      @sorter.expects(:sort_file).with("/downloads/movie.mp3").never
+      @sorter.sort_directory(@download_path)
     end
   end  
   
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,5 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), *%w[.. lib]))
 require 'test/unit'
 require 'shoulda'
-require 'fakefs'
+require 'mocha'
+require 'fakefs/safe'
