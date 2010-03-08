39759e5b5bc7632135d69ff7c6da30f073fd019a
Luke Redpath <luke@lukeredpath.co.uk>
2010-03-08 16:56:00 +0000

    Check that the tv show file was parsed before moving it

--- lib/moviesort/sorter.rb
+++ lib/moviesort/sorter.rb
@@ -16,10 +16,11 @@ module Moviesort
       end
       
       def sort_file(path)
-        tv_show = Moviesort::TVShow.parse(File.basename(path))
-        target_path = File.join(@output_dir, tv_show.target_path)
-        FileUtils.mkdir_p(target_path)
-        FileUtils.mv(path, File.join(target_path, tv_show.target_filename))
+        if tv_show = Moviesort::TVShow.parse(File.basename(path))
+          target_path = File.join(@output_dir, tv_show.target_path)
+          FileUtils.mkdir_p(target_path)
+          FileUtils.mv(path, File.join(target_path, tv_show.target_filename))
+        end
       end
       
       private
